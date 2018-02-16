cdb67ffc872cf184c00a7ea1f425991c869ce317
DanielGunna <danielgunna1408@gmail.com>
2018-02-16 15:01:03 -0200

    Check if ANDROID_HOME already set

--- droid/commands/config_env_darwin.sh
+++ droid/commands/config_env_darwin.sh
@@ -3,21 +3,29 @@
 if [ ! -f ~/.bash_profile ]; then
   echo '.bash_profile not found, creating file.'
   touch ~/.bash_profile
-fi
-
-if [ ! -d "/Users/$USER/Library/Android/sdk" ]; then
-  eval "echo 'Cant locate Android SDK, directory /Users/$USER/Library/Android/sdk does not exists'"
-  echo 'Exiting with error : status 1'
-  exit 1;
 else
-  echo 'export ANDROID_HOME=/Users/$USER/Library/Android/sdk' >> ~/.bash_profile
-  echo 'export PATH=${PATH}:$ANDROID_HOME/plataform-tools/' >> ~/.bash_profile
-
-  echo '
-  # Get the aliases and functions
-  if [ -f ~/.bashrc ]; then
-      . ~/.bashrc
-  fi' >> ~/.bash_profile
-
-  source ~/.bash_profile
+  echo 'Editing existing bash_profile file.'
 fi
+
+if [ -z "$ANDROID_HOME" ]; then
+    echo 'Need to set android  environment variable'
+    if [ ! -d "/Users/$USER/Library/Android/sdk" ]; then
+      eval "echo 'Cant locate Android SDK, directory /Users/$USER/Library/Android/sdk does not exists'"
+      echo 'Exiting with error : status 1'
+      exit 1;
+    else
+      echo 'Setting ANDROID_HOME variable.'
+      echo 'export ANDROID_HOME=/Users/$USER/Library/Android/sdk' >> ~/.bash_profile
+      echo 'export PATH=${PATH}:$ANDROID_HOME/plataform-tools/' >> ~/.bash_profile
+    fi
+else
+    echo 'ANDROID_HOME variable already set.'
+fi
+
+echo '
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+fi' >> ~/.bash_profile
+
+source ~/.bash_profile
